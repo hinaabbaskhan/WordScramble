effd35a2e2ab7365a451ddbf2ee8102516476922
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-04-20 22:40:08 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Hina Khan on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
