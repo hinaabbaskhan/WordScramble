d3b473499cea5fc2d9f79a48c7d7a47c2ec10ac7
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-04-24 14:19:01 +0500

    check if the work entered is real

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -68,6 +68,23 @@ struct ContentView: View {
         // If were are *here* then there was a problem – trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
     }
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
